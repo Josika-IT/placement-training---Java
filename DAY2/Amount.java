f858958a245fa19e733463e51ba9152abeae9540
Josika-IT <josi64178@gmail.com>
2026-07-28 11:40:12 +0530

    Update Amount.java

--- DAY2/Amount.java
+++ DAY2/Amount.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args){
-	    Scanner sc = new Scanner(System.in);
-	    System.out.print("Price: ");
-	    int price = sc.nextInt();
-	    System.out.print("Quantity: ");
-	    int quantity = sc.nextInt();
-	   
-	    int amount = price*quantity;
-	    System.out.println("Amount: "+amount);
-	}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Price: ");
+        int price = sc.nextInt();
+
+        System.out.print("Quantity: ");
+        int quantity = sc.nextInt();
+
+        System.out.print("Discount (%): ");
+        int per = sc.nextInt();
+
+        int amount = price * quantity;
+        int discount = amount * per / 100;
+        int finalAmount = amount - discount;
+
+        System.out.println("Actual Amount: " + amount);
+        System.out.println("Discount Amount: " + discount);
+        System.out.println("Final Amount: " + finalAmount);
+
+        sc.close();
+    }
 }
